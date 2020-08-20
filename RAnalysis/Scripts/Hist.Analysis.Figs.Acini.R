9ce46e80a53a61a9c44616464d6700693ad23bd6
SamJGurr <samjgurr@gmail.com>
2020-08-20 13:02:46 -0400

    added anova table, figures, and script edits

--- RAnalysis/Scripts/Hist.Analysis.Figs.Acini.R
+++ RAnalysis/Scripts/Hist.Analysis.Figs.Acini.R
@@ -13,7 +13,6 @@ library(ggplot2)        # Version 2.2.1, Packaged: 2016-12-30, Depends: R (>= 3.
 library(tidyr)
 library(ggpubr)
 library(car)
-
 #set working directory---------------------------------------------------------------------------------
 setwd("C:/Users/samjg/Documents/My_Projects/Pgenerosa_histology/RAnalysis/Data") #set working
 
@@ -42,22 +41,26 @@ Means_Table <- dat2 %>%
             mean_zoa= mean(zoa, na.rm = TRUE),
             mean_cytes = mean(cytes, na.rm = TRUE),
             mean_lumen= mean(lumen, na.rm = TRUE),
-            mean_perc_zoa= mean(perc_zoa, na.rm = TRUE),
-            mean_perc_cytes = mean(perc_cytes, na.rm = TRUE),
-            mean_perc_lumen= mean(perc_lumen, na.rm = TRUE),
-            mean_zoa_cyte_ratio = mean(zoa_cyte_ratio, na.rm = TRUE),
+            mean_total_acini_area = mean(total_area, na.rm = TRUE),
             num_acini=n())
 Means_Table # view the table
 
 # PIVOT LONGER-----------------------------------------------------------------------------------------
 Means_Table_long <- Means_Table %>% 
-  tidyr::pivot_longer(cols = c(4:10), names_to='scoring_metric', values_to='means')
+  tidyr::pivot_longer(cols = c(4:7), names_to='scoring_metric', values_to='means')
 
 #  PLOTS----------------------------------------------------------------------------------------
+# ocrete variable Date_Treat
 Means_Table_long$Date_Treat <- paste((substr(Means_Table_long$Date,5,8)),Means_Table_long$Treatment, sep ="_")
+list(Means_Table_long$Date_Treat)
+# order the levels of the factor date_treatment to order correly in the plot
+Means_Table_long$Date_Treat <- factor(Means_Table_long$Date_Treat,levels = c("0123_Ambient", "0123_Low", "0221_Ambient", "0221_Low"))
 
+
+
+# plots
 plot <- ggboxplot(Means_Table_long, x = "Date_Treat", y = "means",  fill = "Treatment",
-                     palette = c("#FC4E07","#00AFBB"), add = "none")
+                     palette = c("#00AFBB", "#FC4E07"), add = "none")
 plot2 <- plot %>% ggadd(shape ="Treatment",fill = "white") %>% ggadd("jitter", size = 4,shape ="Treatment",fill = "white") +
   facet_wrap( ~ scoring_metric, ncol=2, scales = "free") + theme_classic()
 plot2
@@ -69,6 +72,17 @@ par(mfrow=c(1,4)) #set plotting configuration
 par(mar=c(1,1,1,1)) #set margins for plots
 
 # Two-Way ANOVA (treat×time)
+typeof(Means_Table$Date) # currenty an integer - must change to a character
+Means_Table$Date <- as.character(Means_Table$Date)
+## total acini area test---------------------------------
+mod_total_acini_TIME  <- aov(mean_total_acini_area~Treatment*Date, data = Means_Table)
+anova(mod_total_acini_TIME) # p = 0.9391
+shapiro.test(residuals(mod_total_acini_TIME)) #  normal residuals p-value = 7.261e-06
+leveneTest(mod_total_acini_TIME) # p = 0.6463
+hist(residuals(mod_total_acini_TIME)) #plot histogram of residuals
+boxplot(residuals(mod_total_acini_TIME)) #plot boxplot of residuals
+plot(fitted(mod_total_acini_TIME),residuals(mod_total_acini_TIME))
+qqnorm(residuals(mod_total_acini_TIME)) # qqplot
 
 ## cytes test---------------------------------
 mod_cytes_TIME  <- aov(mean_cytes~Treatment*Date, data = Means_Table)
@@ -103,54 +117,3 @@ plot(fitted(mod_lumen_TIME),residuals(mod_lumen_TIME))
 qqnorm(residuals(mod_lumen_TIME)) # qqplot
 
 TukeyHSD(mod_lumen_TIME)
-
-# One-Way anova
-## cytes test---------------------------------
-mod_cytes  <- aov(mean_lumen~Treatment*Date, data = Means_Table)
-anova(mod_cytes) # p = 0.9391
-shapiro.test(residuals(mod_cytes)) #  normal residuals p-value = 0.3263
-leveneTest(mod_cytes) # p = 0.6256
-hist(residuals(mod_cytes)) #plot histogram of residuals
-boxplot(residuals(mod_cytes)) #plot boxplot of residuals
-plot(fitted(mod_cytes),residuals(mod_cytes))
-qqnorm(residuals(mod_zoa)) # qqplot
-
-
-### zoa test---------------------------------
-anova(mod_zoa) # p = 0.494
-shapiro.test(residuals(mod_zoa)) #  normal residuals p-value = 0.2042
-leveneTest(mod_zoa) # p = 0.5209
-hist(residuals(mod_zoa)) #plot histogram of residuals
-boxplot(residuals(mod_zoa)) #plot boxplot of residuals
-plot(fitted(mod_zoa),residuals(mod_zoa))
-qqnorm(residuals(mod_zoa)) # qqplot
-
-## cytes test---------------------------------
-mod_cytes  <- aov(mean_perc_cytes~Treatment, data = Means_Table)
-anova(mod_cytes) # p = 0.9391
-shapiro.test(residuals(mod_cytes)) #  normal residuals p-value = 0.3263
-leveneTest(mod_cytes) # p = 0.6256
-hist(residuals(mod_cytes)) #plot histogram of residuals
-boxplot(residuals(mod_cytes)) #plot boxplot of residuals
-plot(fitted(mod_cytes),residuals(mod_cytes))
-qqnorm(residuals(mod_zoa)) # qqplot
-
-## lumen test---------------------------------
-mod_lumen  <- aov(mean_perc_lumen~Treatment, data = Means_Table)
-anova(mod_lumen) # p = 0.1876
-shapiro.test(residuals(mod_lumen)) #  normal residuals p-value = 0.4007
-leveneTest(mod_lumen) # p = 0.6256
-hist(residuals(mod_lumen)) #plot histogram of residuals
-boxplot(residuals(mod_lumen)) #plot boxplot of residuals
-plot(fitted(mod_lumen),residuals(mod_lumen))
-qqnorm(residuals(mod_lumen)) # qqplot
-
-## zoa : cytes test---------------------------
-mod_zoa_cytes <- aov(mean_zoa_cyte_ratio~Treatment, data = Means_Table)
-anova(mod_zoa_cytes) # p = 0.8147
-shapiro.test(residuals(mod_zoa_cytes)) #  normal residuals p-value = 0.4334
-leveneTest(mod_zoa_cytes) # p = 0.4916
-hist(residuals(mod_zoa_cytes)) #plot histogram of residuals
-boxplot(residuals(mod_zoa_cytes)) #plot boxplot of residuals
-plot(fitted(mod_zoa_cytes),residuals(mod_zoa_cytes))
-qqnorm(residuals(mod_zoa_cytes)) # qqplot
