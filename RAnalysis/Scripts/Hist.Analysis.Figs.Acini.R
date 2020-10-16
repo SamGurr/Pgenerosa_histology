42d022080f4ee51d3b99e9c43324ff32cdb47833
SamJGurr <samjgurr@gmail.com>
2020-10-16 16:34:44 -0400

    added male hist photos and updated analysis script of acinus

--- RAnalysis/Scripts/Hist.Analysis.Figs.Acini.R
+++ RAnalysis/Scripts/Hist.Analysis.Figs.Acini.R
@@ -18,24 +18,79 @@ library(gridExtra)
 setwd("C:/Users/samjg/Documents/My_Projects/Pgenerosa_histology/RAnalysis/Data") #set working
 
 # UPLOAD DATA------------------------------------------------------------------------------------------
-dat<-read.csv("Master_summary_male_acini.csv", header=T, sep=",", na.string="NA", as.is=T) 
-dat # view data
-dat$Area = as.numeric(dat$Area)
+Male_hist<-read.csv("Master_summary_male_acini.csv", header=T, sep=",", na.string="NA", as.is=T) 
+Male_hist # view data
+Male_hist$Area = as.numeric(dat$Area)
 
 # PIVOT THE TABLE TO CALC RELATIVE VALUES--------------------------------------------------------------
-dat2 <- dat %>% dplyr::select(-c('Meas_num','Label','hue','saturation','brightness')) %>% 
+Male_hist_2 <- Male_hist %>% dplyr::select(-c('Meas_num','Label','hue','saturation','brightness')) %>% 
   tidyr::pivot_wider(names_from=type, values_from=Area)
 
 # CALC RELATIVE VALUES---------------------------------------------------------------------------------
-dat2$TOTAL_AREA <- (dat2$cytes_zoa + dat2$lumen)
-dat2$perc_zoa <- (dat2$zoa/dat2$TOTAL_AREA)*100 # percent area of spermatozoa
-dat2$perc_cytes <- ((dat2$cytes_zoa - dat2$zoa)/dat2$TOTAL_AREA)*100 # percent area of spermatocytes
-dat2$perc_lumen <- (dat2$lumen/dat2$TOTAL_AREA)*100 # percent area of lumen
-dat2$zoa_cyte_ratio <- (dat2$zoa/(dat2$cytes_zoa - dat2$zoa)) # ratio of spermatozoa : spermatocytes
-dat2$cytes <- (dat2$cytes_zoa - dat2$zoa) # area of spermatocytes
+Male_hist_2$perc_zoa <- (Male_hist_2$zoa/Male_hist_2$total_area)*100 # percent area of spermatozoa
+Male_hist_2$perc_cytes <- ((Male_hist_2$cytes_zoa - Male_hist_2$zoa)/Male_hist_2$total_area)*100 # percent area of spermatocytes
+Male_hist_2$perc_lumen <- (Male_hist_2$lumen/Male_hist_2$total_area)*100 # percent area of lumen
+Male_hist_2$cytes <- (Male_hist_2$cytes_zoa - Male_hist_2$zoa) # area of spermatocytes
+
+# convert to characer
+typeof(Male_hist_2$ID) 
+typeof(Male_hist_2$Date) 
+Male_hist_2$Date <- as.character(Male_hist_2$Date)
+
+# call subset for plotting and pivot longer with tidyr
+colnames(Male_hist_2)
+Male_hist_plots <- Male_hist_2[,c(1:3,9:11)]
+
+Male_hist_plots_long <- Male_hist_plots %>% 
+  tidyr::pivot_longer(cols = c(4:6), names_to='prop_metric', values_to='value')
+
+
+# change labels for plotting with gsub
+Male_hist_plots_long$Date <- gsub("20190123", "72",Male_hist_plots_long$Date)
+Male_hist_plots_long$Date <- gsub("20190221", "93 + 8 day recovery",Male_hist_plots_long$Date)
+
+Male_hist_plots_long$prop_metric <- gsub("perc_cytes", "spermatocytes",Male_hist_plots_long$prop_metric)
+Male_hist_plots_long$prop_metric <- gsub("perc_zoa", "spermatozoa",Male_hist_plots_long$prop_metric)
+Male_hist_plots_long$prop_metric <- gsub("perc_lumen", "lumen",Male_hist_plots_long$prop_metric)
+
+#convert metric to ordered factor
+Male_hist_plots_long_ordered <- Male_hist_plots_long[order(Male_hist_plots_long$prop_metric ),]
+
+Male_hist_plots_long_ordered$prop_metric <- factor(Male_hist_plots_long_ordered$prop_metric, levels = rev(c(rep("lumen",1), rep("spermatocytes",1), rep("spermatozoa",1))))
+
+
+Male_hist_plots_long_ordered_2 <- Male_hist_plots_long_ordered%>%
+  group_by(Date, Treatment, prop_metric) %>% 
+  summarise(mean = mean(value), sd =sd(value)) %>% 
+  mutate(y_pos = cumsum(mean))
+
+
+# stacked proportion plot
+pd <- position_dodge2(width = 0.2)
+
+Male_hist_plots_long_ordered_2$prop_metric <- as.character(Male_hist_plots_long_ordered_2$prop_metric)
+
+ggplot(Male_hist_plots_long_ordered_2, aes(x = Treatment, y = mean, fill = Treatment, alpha = prop_metric)) + 
+  geom_bar(stat = "identity") + 
+  geom_errorbar(data = Male_hist_plots_long_ordered_2, mapping=aes(ymax = y_pos + sd, ymin=y_pos - sd), stat = "identity", width = 0.1, alpha = 0.7, position = pd) + 
+  facet_wrap(~Date)+ scale_alpha_manual(values=c(seq(0.3,1, length.out = 3))) + 
+  theme_bw() + 
+  scale_y_continuous(breaks = seq(from = 0, to = 100, by = 10)) + 
+  ylab("mean proportion of total acini area (%)") + 
+  theme(axis.title.x = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank()) 
+dev.off()
+
+
+
+################################################## #
+################################################## #
+######   CONVERT TO MEANS TO TACKLE  ############# #
+################################################## #
+########MIGHT BE THE WRONG APPROACH ############## #
+
 
 # CALC THE MEAN FOR EACH SAMPLE-------------------------------------------------------------------------
-Means_Table <- dat2 %>%
+Means_Table <- Male_hist_2 %>%
   dplyr::select(-'acini_segment') %>% # remove unecessary columns
   group_by(ID,Treatment,Date) %>%
   summarize(
@@ -105,9 +160,50 @@ pD <- plot_lumen_proportion %>% ggadd("jitter", size = 3, fill = "white") + them
 Final_Plot_Grid <- grid.arrange(pA, pB,pC, pD, ncol =2, nrow = 2)
 Final_Plot_Grid # view plot
 
+# Fig Grid 2
+Means_Table[11,]
+MeansTable_2 <- Means_Table[-c(11), ]
+# plot  spermatozoa 
+plot_zoa_area <- ggplot(MeansTable_2, aes(x=Date, y=mean_zoa, color=Treatment, fill - white)) +
+  theme_bw() +
+  geom_boxplot() +
+  ylab("mean area spermatozoa (µm^3)") +
+  scale_x_discrete(name ="exposure time (days)", labels=c("72","93 + 8 day recovery")) +
+  annotate("text", size = 12, x=0.55, y=320, label= "B")
+pB2 <- plot_zoa_area %>% ggadd("jitter", size = 3, fill = "white") + theme(legend.position="none")
+# plot  spermatocytes 
+plot_cytes_area <- ggplot(MeansTable_2, aes(x=Date, y=mean_cytes, color=Treatment, fill - white)) +
+  theme_bw() +
+  geom_boxplot() +
+  ylab("mean area spermatocytes (µm^3)") +
+  scale_x_discrete(name ="exposure time (days)", labels=c("72","93 + 8 day recovery")) +
+  annotate("text", size = 12, x=0.55, y=975, label= "C")
+pC2 <- plot_cytes_area %>% ggadd("jitter", size = 3, fill = "white") + theme(legend.position="none")
+# plot  lumen
+plot_lumen_area <- ggplot(MeansTable_2, aes(x=Date, y=mean_lumen, color=Treatment, fill - white)) +
+  theme_bw() +
+  geom_boxplot() +
+  ylab("mean area lumen (µm^3)") +
+  scale_x_discrete(name ="exposure time (days)", labels=c("72","93 + 8 day recovery")) +
+  annotate("text", size = 12, x=0.55, y=200, label= "D")
+pD2 <- plot_lumen_area %>% ggadd("jitter", size = 3, fill = "white") + theme(legend.position="none")
+# gird plots
+Final_Plot_Grid2 <- grid.arrange(pA, pB2,pC2, pD2, ncol =2, nrow = 2)
+Final_Plot_Grid2 # view plot
+
+#  SAVE
+ggsave(file="Grid_plot.pdf", Final_Plot_Grid2, width = 12, height = 8, units = c("in")) 
 
 
-# STATISTICS------------------------------------------------------------------------------------------
+
+
+################################################################# #
+################################################################# #
+# STATISTICS WITH MEAN DATA------------------------------------------------------------------------------------------
+################################################################# #
+################################################################# #
+################################################################# #
+
 Means_Table$Treatment <- as.factor(Means_Table$Treatment)
 Means_Table$Date <- as.factor(Means_Table$Date)
 par(mfrow=c(2,2)) #set plotting configuration
@@ -169,7 +265,7 @@ plot(fitted(mod_total_acini_TIME),residuals(mod_total_acini_TIME))
 qqnorm(residuals(mod_total_acini_TIME)) # qqplot
 
 ## cytes test---------------------------------
-mod_cytes_TIME  <- aov(mean_cytes~Treatment*Date, data = Means_Table)
+mod_cytes_TIME  <- aov(mean_cytes~Treatment*Date, data = MeansTable_2)
 anova(mod_cytes_TIME) # p = 0.9391
 shapiro.test(residuals(mod_cytes_TIME)) #  normal residuals p-value = 7.261e-06
 leveneTest(mod_cytes_TIME) # p = 0.6463
@@ -179,7 +275,7 @@ plot(fitted(mod_cytes_TIME),residuals(mod_cytes_TIME))
 qqnorm(residuals(mod_cytes_TIME)) # qqplot
 
 ## zoa test---------------------------------
-mod_zoa_TIME  <- aov(mean_zoa~Treatment*Date, data = Means_Table)
+mod_zoa_TIME  <- aov(mean_zoa~Treatment*Date, data = MeansTable_2)
 anova(mod_zoa_TIME) # p = 0.01051 * (time)
 shapiro.test(residuals(mod_zoa_TIME)) #  normal residuals p-value = 0.08541
 leveneTest(mod_zoa_TIME) # p = 0.1719
@@ -191,7 +287,7 @@ qqnorm(residuals(mod_zoa_TIME)) # qqplot
 TukeyHSD(mod_zoa_TIME)
 
 ## lumen test---------------------------------
-mod_lumen_TIME  <- aov(mean_lumen~Treatment*Date, data = Means_Table)
+mod_lumen_TIME  <- aov(mean_lumen~Treatment*Date, data = MeansTable_2)
 anova(mod_lumen_TIME) # p = 0.06423 . (treat) ; 0.09255 . (time)
 shapiro.test(residuals(mod_lumen_TIME)) #  normal residuals p-value = 0.2685
 leveneTest(mod_lumen_TIME) # p = 0.1227
